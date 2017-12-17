e2732e994cdad13b93ac7e0ae7cf70acfc8b2651
MilanSas <milansas1@gmail.com>
2017-12-17 17:24:38 +0100

    initial commit

---
+++ BinaryTree/BinaryTree/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node<int> BinaryTree = new EmptyNode<int>();
+
+            BinaryTree = BinaryTree.Insert(6);
+            BinaryTree = BinaryTree.Insert(8);
+            BinaryTree = BinaryTree.Insert(7);
+            BinaryTree = BinaryTree.Insert(3);
+            BinaryTree = BinaryTree.Insert(5);
+            BinaryTree = BinaryTree.Insert(4);
+            BinaryTree.printnodes();
+            Node<int> Searchednode = BinaryTree.Search(0);
+            Searchednode.printnodes();
+            BinaryTree = BinaryTree.Delete(6);
+            BinaryTree.printnodes();
+
+
+        }
+    }
+}
